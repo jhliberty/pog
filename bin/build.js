d70bae6348892c4446d7c25e6fdc5886ceacd3bb
Jesse Weed <jesse@jesseweed.com>
2015-02-25 15:51:12 -0800

    update to latest pog core

--- bin/build.js
+++ bin/build.js
@@ -165,6 +165,9 @@ module.exports = function ( program ) {
                                 wrench.rmdirSyncRecursive(path + '/app/views');
                                 wrench.copyDirSyncRecursive(__dirname + '/_src/lib/templates/views/nunjucks', path + '/app/views/');
 
+                                wrench.rmdirSyncRecursive(path + '/app/controllers');
+                                wrench.copyDirSyncRecursive(__dirname + '/_src/lib/controllers/handlebars', path + '/app/controllers');
+
                                 // UPDATE SCRIPT FILE
                                 scriptFile = path + '/app/views/_inc/footer.nj';
                                 scriptContent = fs.readFileSync(scriptFile);
